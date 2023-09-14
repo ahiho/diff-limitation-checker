facf37c8a231e5944c618d899c55cd4e08d749a7
Long Le <levanlongktmt@gmail.com>
2023-09-14 14:39:31 +0700

    add logic code

--- dist/index.js
+++ dist/index.js
@@ -2688,19 +2688,19 @@ exports["default"] = _default;
 
 /***/ }),
 
+/***/ 716:
+/***/ ((module) => {
+
+module.exports = eval("require")("@actions/github");
+
+
+/***/ }),
+
 /***/ 491:
 /***/ ((module) => {
 
 "use strict";
 module.exports = require("assert");
-
-/***/ }),
-
-/***/ 81:
-/***/ ((module) => {
-
-"use strict";
-module.exports = require("child_process");
 
 /***/ }),
 
@@ -2826,50 +2826,60 @@ var __webpack_exports__ = {};
 // This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
 (() => {
 const core = __nccwpck_require__(186);
-const { exec } = __nccwpck_require__(81);
+const { context, GitHub, getOctokit } = __nccwpck_require__(716);
 
 // most @actions toolkit packages have async methods
 async function run() {
   try {
-    const targetBranch = core.getInput("target-branch");
-    const change = await new Promise((resolve, reject) => {
-      exec(
-        `git --no-pager diff --shortstat origin/${targetBranch}`,
-        (err, stdout, stderr) => {
-          if (!!err) {
-            reject(err);
-            return;
-          }
-          if (!!stderr) {
-            reject(stderr);
-            return;
-          }
-          resolve(stdout);
-        }
-      );
+    const base = context.payload.pull_request.base.sha;
+    const head = context.payload.pull_request.head.sha;
+
+    const client = new GitHub(core.getInput("token", { required: true }));
+    // Use GitHub's compare two commits API.
+    // https://developer.github.com/v3/repos/commits/#compare-two-commits
+    const response = await client.repos.compareCommits({
+      base,
+      head,
+      owner: context.repo.owner,
+      repo: context.repo.repo,
     });
 
-    if (!change) {
-      return;
+    // Ensure that the request was successful.
+    if (response.status !== 200) {
+      core.setFailed(
+        `The GitHub API for comparing the base and head commits for this ${context.eventName} event returned ${response.status}, expected 200. ` +
+          "Please submit an issue on this action's GitHub repo."
+      );
     }
-    const idx = change.indexOf("files changed");
-    if (idx <= 0) {
-      return;
-    }
-    const changeCount = Number(change.substring(0, idx).replace(/ |\n/g, ""));
-    const maxFile = Number(core.getInput("max-file"));
-    const token = core.getInput("token");
-    const octokit = github.getOctokit(token);
 
-    const [owner, repo] = core.getInput("repository").split("/");
+    // Ensure that the head commit is ahead of the base commit.
+    if (response.data.status !== "ahead") {
+      core.setFailed(
+        `The head commit for this ${context.eventName} event is not ahead of the base commit. ` +
+          "Please submit an issue on this action's GitHub repo."
+      );
+    }
+
+    const files = response.data.files;
+
+    let changeCount = 0;
+    for (const file of files) {
+      if (file.status === "added" || file.status === "modified") {
+        changeCount += 1;
+      }
+    }
+    const token = core.getInput("token");
+    const octokit = getOctokit(token);
+
+    const maxFile = Number(core.getInput("max-file"));
 
     if (changeCount > maxFile) {
       const msg = `PR contains has more than ${maxFile} files changed, pls explain`;
       octokit.rest.issues.createComment({
         body: msg,
-        repo,
-        owner,
-        issue_number: Number(core.getInput("issue-number")),
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+        issue_number: context.payload.pull_request.number,
       });
       core.setFailed(msg);
     } else {
